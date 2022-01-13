066eeddf6a28c59b109f2b7138784e5ac1851a9d
Tyler <tylerbarrett2010+github@gmail.com>
2022-01-13 00:19:29 -0500

    Wow this actually looks like it's 1% of the way there
    Also some decent ideas for the overall design

--- app/src/main/java/com/example/headunitapplication/MainActivity.java
+++ app/src/main/java/com/example/headunitapplication/MainActivity.java
@@ -11,4 +11,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
     }
+
+    /* "Widgets" to have:
+    - Current song / artist
+    - "Engine effort" (RPM)
+    - Vehicle speed
+    - Battery level?
+    - Engine temperature?
+    - Current posted limit? (use OSM API)
+    - "Safety distance" (Distance to next speed camera)
+
+    Additional things to have:
+    - Idle animation
+    - Depth of field effect? (blur some elements while others are in focus?)
+    - put OSM streets on the map in a 3D view? (ooh yeah that's a good idea)
+     */
 }
