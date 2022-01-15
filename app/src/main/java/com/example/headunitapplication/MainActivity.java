8c918eab4aa1e2a14b8b138defa8e912de986df0
Tyler <tylerbarrett2010+github@gmail.com>
2022-01-14 23:03:47 -0500

    Added Google Maps Functionality
    We can now see a 3D view of buildings in the area using Google Maps

--- app/src/main/java/com/example/headunitapplication/MainActivity.java
+++ app/src/main/java/com/example/headunitapplication/MainActivity.java
@@ -1,15 +1,57 @@
 package com.example.headunitapplication;
 
 import android.os.Bundle;
+import android.util.Log;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    public static final CameraPosition SYDNEY =
+            new CameraPosition.Builder().target(new LatLng(-33.87365, 151.20689))
+                    .zoom(18)
+                    .bearing(0)
+                    .tilt(45)
+                    .build();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        boolean success = googleMap.setMapStyle(
+                MapStyleOptions.loadRawResourceStyle(
+                        this, R.raw.map_style));
+        googleMap.setIndoorEnabled(false);
+
+        if (!success) {
+            System.err.println("Unable to parse style JSON.");
+        }
+
+//        googleMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("Marker"));
+
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(SYDNEY), null);
     }
 
     /* "Widgets" to have:
---
+++ app/src/main/java/com/example/headunitapplication/openstreetmap/OpenStreetMap.java
@@ -0,0 +1,2 @@
+package com.example.headunitapplication.openstreetmap;public class OpenStreetMap {
+}
