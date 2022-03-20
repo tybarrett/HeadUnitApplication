5f72d901a0f8f428c3c5bda64d1b224cdf560335
Tyler <tylerbarrett2010+github@gmail.com>
2022-03-20 02:16:41 -0400

    We Now Rotate a Little Better :)

--- app/src/main/java/com/example/headunitapplication/views/MapRotator.java
+++ app/src/main/java/com/example/headunitapplication/views/MapRotator.java
@@ -11,7 +11,7 @@ import com.google.android.gms.maps.model.LatLng;
 
 public class MapRotator extends Thread {
 
-    private static final int ROTATION_PERIOD_MS = 30 * 1000;
+    private static final int ROTATION_SPEED_FACTOR = 10;
 
     private GoogleMap map = null;
     private long initTime = 0;
@@ -43,12 +43,9 @@ public class MapRotator extends Thread {
 
                         float tilt = map.getCameraPosition().tilt;
                         float zoom = map.getCameraPosition().zoom;
+                        float oldBearing = map.getCameraPosition().bearing;
 
-                        long timeAlive = System.currentTimeMillis() - initTime;
-                        int bearing = (int) (timeAlive * 360 / ROTATION_PERIOD_MS);
-                        bearing = (bearing % 360) - 180;
-                        System.out.println(bearing);
-
+                        int bearing = (int) (oldBearing + ROTATION_SPEED_FACTOR);
 
                         CameraPosition rotatedPosition = new CameraPosition.Builder().
                                 target(currentTarget).
@@ -57,13 +54,13 @@ public class MapRotator extends Thread {
                                 tilt(tilt).
                                 build();
 
-                        map.moveCamera(CameraUpdateFactory.newCameraPosition(rotatedPosition));
+                        map.animateCamera(CameraUpdateFactory.newCameraPosition(rotatedPosition));
                     }
                 });
             }
 
             try {
-                Thread.sleep(25);
+                Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
